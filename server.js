1ae6554086eb40ded58e71f4c44b8a6987697c70
Troy <tydamon@hotmail.com>
2024-02-19 16:50:17 +1100

    added to api requests

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 // Middleware for body parsing 
 app.use(express.static('public'));
@@ -17,8 +17,12 @@ app.use(express.json());
 
 // GET //
 
+app.get('api/notes/:id', (req, res) =>{
+  res.json(notes[req.params.id]);
+});
+
 app.get('/api/notes', (req, res) => {
-  fs.readFile('./db/db.json', (err, data) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
     if (err) throw err;
     var notes = JSON.parse(data);
     res.json(notes);
@@ -28,26 +32,30 @@ app.get('/api/notes', (req, res) => {
 // POST //
 
 app.post('/api/notes', (req, res) => {
-
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
     var notes = JSON.parse(data);
     let userNote = req.body;
     userNote.id = Math.floor(Math.random() * 5000);
     notes.push(userNote);
   fs.writeFile('./db/db.json', JSON.stringify(notes), (err, data) => {
       res.json(userNote);
-  });
+    });
+  }); 
 });
 
 // DELETE //
 
 app.delete('/api/notes/:id', (req, res) => {
-
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
     let notes = JSON.parse(data);
     const newNotes = notes.filter(note => note.id !== parseInt(req.params.id));
   
   fs.writeFile('./db/db.json', JSON.stringify(newNotes), (err, data) => {
-    res.json({msg: 'successfully'});
+    res.json({msg: 'note deleted'});
   });
+});
 });
 
 //=======================//
